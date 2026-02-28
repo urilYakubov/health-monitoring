4b52b185d909f302ca078104d6121bf94d5b6245
urilYakubov <uyakubov007@gmail.com>
2026-02-28 09:48:44 +0200

    Updated logger.js according to vercel serverless production server

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,5 +1,7 @@
 const { createLogger, format, transports } = require("winston");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const logger = createLogger({
   level: "info",
   format: format.combine(
@@ -7,18 +9,21 @@ const logger = createLogger({
     format.errors({ stack: true }),
     format.json()
   ),
-  defaultMeta: { service: "health-dashboard-api" },
   transports: [
     new transports.Console(),
 
-    new transports.File({
-      filename: "logs/error.log",
-      level: "error"
-    }),
-
-    new transports.File({
-      filename: "logs/combined.log"
-    })
+    // Only use file logging locally
+    ...(!isProduction
+      ? [
+          new transports.File({
+            filename: "logs/error.log",
+            level: "error"
+          }),
+          new transports.File({
+            filename: "logs/app.log"
+          })
+        ]
+      : [])
   ]
 });
 
